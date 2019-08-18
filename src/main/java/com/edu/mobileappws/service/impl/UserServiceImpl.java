b18ae5e17c7576d649d91535459107065422280a
icarus8050 <icarus8050@naver.com>
2019-08-18 23:37:25 +0900

    AuthenticationFilter 및 JWT 토큰 구현

--- src/main/java/com/edu/mobileappws/service/impl/UserServiceImpl.java
+++ src/main/java/com/edu/mobileappws/service/impl/UserServiceImpl.java
@@ -7,10 +7,13 @@ import com.edu.mobileappws.shared.Utils;
 import com.edu.mobileappws.shared.dto.UserDto;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -47,7 +50,13 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        UserEntity userEntity = userRepository.findByEmail(email);
+
+        if (userEntity == null) {
+            throw new UsernameNotFoundException(email);
+        }
+
+        return new User(userEntity.getEmail(), userEntity.getEncryptPassword(), new ArrayList<>());
     }
 }
