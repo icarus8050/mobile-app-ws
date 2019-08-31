76ec527a6a3e836f8f5a000a0177011ec18b9e8b
icarus8050 <icarus8050@naver.com>
2019-09-01 01:25:31 +0900

    TokenSecret를 코드에서 하드코딩 하는 방법에서 properties 에서 읽어오는 방법

--- src/main/java/com/edu/mobileappws/security/SecurityConstants.java
+++ src/main/java/com/edu/mobileappws/security/SecurityConstants.java
@@ -1,9 +1,15 @@
 package com.edu.mobileappws.security;
 
+import com.edu.mobileappws.SpringApplicationContext;
+
 public class SecurityConstants {
     public static final long EXPIRATION_TIME = 864_000_000;   //10 days
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
     public static final String SIGN_UP_URL = "/users";
-    public static final String TOKEN_SECRET = "jf9i4jgu83nfl0";
+
+    public static String getTokenSecret() {
+        AppProperties appProperties = (AppProperties) SpringApplicationContext.getBean("AppProperties");
+        return appProperties.getTokenSecret();
+    }
 }
